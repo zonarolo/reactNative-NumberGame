cc61a38b2015d6c5f6fc25dbc264a7c8a456e352
Juan Rolo <juan-luis.rolo@inetum.com>
2022-10-19 09:09:55 +0200

    Add solution to the problems

--- components/Input.js
+++ components/Input.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import { StyleSheet, TextInput} from 'react-native';
+import { TextInput} from 'react-native';
 
-const Input = props => 
-  <TextInput style={{...styles.input, ...props.style}} {...props}/>
-;
-
-const styles = StyleSheet.create({
-  input: {
-    height: 30,
-    borderBottomColor: 'grey',
-    borderBottomWidth: 1,
-    marginVertical: 10
-  }
-})
+const Input = props => <TextInput style={{...props.style}} {...props}/>;
 
 export default Input;
--- pages/StartGameScreen.js
+++ pages/StartGameScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 import Card from '../components/Card';
@@ -6,24 +6,33 @@ import Colors from '../constants/colors';
 import Input from '../components/Input';
 
 const StartGameScreen = () => {
+  const [input, setInput] = useState();
+
+  
+  const handleInput = () => {
+    console.log(input);
+  }
+
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Comenzar Juego</Text>
       <Card style={styles.inputContainer}>
         <Text>Elija un numero</Text>
-        <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType='numeric' maxLength={2} />
+        <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType='numeric' maxLength={2} onChangeText={newInput => setInput(newInput)}
+        defaultValue={input}/>
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title='Limpiar' color={Colors.accent} onPress={() => {}} />
+            <Button title='Limpiar' color={Colors.accent} onPress={() => setInput()} />
           </View>
           <View style={styles.button}>
-            <Button title='Confirmar' color={Colors.primary} onPress={() => {}} />
+            <Button title='Confirmar' color={Colors.primary} onPress={handleInput} />
           </View>
         </View>
       </Card>
     </View>
   )
 }
+
 
 const styles = StyleSheet.create({
   screen: {
@@ -51,7 +60,13 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15
   },
   input: {
-
+    textAlign: 'center',
+    margin: 15,
+    borderColor: "gray",
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
   }
 })
 
